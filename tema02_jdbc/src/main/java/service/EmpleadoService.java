cecb7259d71baf4809b2f43d44e77e28562810fe
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-07 11:01:25 +0100

    implement logger

--- tema02_jdbc/src/main/java/service/EmpleadoService.java
+++ tema02_jdbc/src/main/java/service/EmpleadoService.java
@@ -7,21 +7,23 @@ import model.EmpleadoDAO;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class EmpleadoService implements EmpleadoDAO {
+
+    private static final Logger log = Logger.getLogger(EmpleadoService.class.getName());
 
     @Override
     public void insertar(Empleado empleado) {
         String sql = "INSERT INTO empleados (nombre_usuario, contrasena, nombre_completo, telefono_contacto) VALUES (?, ?, ?, ?)";
-        try (Connection conn = DatabaseConnection.get();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = DatabaseConnection.get().prepareStatement(sql)) {
             stmt.setString(1, empleado.getNombreUsuario());
             stmt.setString(2, empleado.getContrasena());
             stmt.setString(3, empleado.getNombreCompleto());
             stmt.setString(4, empleado.getTelefonoContacto());
             stmt.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.throwing("EmpleadoService()", "insertar", e );
         }
     }
 
@@ -42,15 +44,34 @@ public class EmpleadoService implements EmpleadoDAO {
 
     @Override
     public Empleado obtenerPorId(int idEmpleado) {
-        return null;
+        Empleado empleado = null;
+        String sql = "SELECT id_empleado, nombre_usuario, contrasena, nombre_completo, telefono_contacto FROM empleados WHERE id_empleado = ?";
+
+        try (PreparedStatement stmt = DatabaseConnection.get().prepareStatement(sql)) {
+
+            stmt.setInt(1, idEmpleado);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    empleado = new Empleado(
+                            rs.getInt("id_empleado"),
+                            rs.getString("nombre_usuario"),
+                            rs.getString("contrasena"),
+                            rs.getString("nombre_completo"),
+                            rs.getString("telefono_contacto"));
+                }
+            }
+
+        } catch (SQLException e) {
+            log.throwing("EmpleadoService()", "insertar", e );
+        }
+        return empleado;
     }
 
     @Override
     public List<Empleado> obtenerTodos() {
         List<Empleado> empleados = new ArrayList<>();
         String sql = "SELECT id_empleado, nombre_usuario, contrasena, nombre_completo, telefono_contacto FROM empleados";
-        try (Connection conn = DatabaseConnection.get();
-             PreparedStatement stmt = conn.prepareStatement(sql);
+        try (PreparedStatement stmt = DatabaseConnection.get().prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
             while (rs.next()) {
                 Empleado empleado = new Empleado(
@@ -62,7 +83,7 @@ public class EmpleadoService implements EmpleadoDAO {
                 empleados.add(empleado);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.throwing("EmpleadoService", "obtenerPorId()", e );
         }
         return empleados;
     }
