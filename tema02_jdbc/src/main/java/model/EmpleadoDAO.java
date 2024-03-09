13d95866ef8063c21d0c880422d072ebc6cb1601
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-09 09:06:13 +0100

    Update EmpleadoDAO.java

--- tema02_jdbc/src/main/java/model/EmpleadoDAO.java
+++ tema02_jdbc/src/main/java/model/EmpleadoDAO.java
@@ -2,6 +2,18 @@ package model;
 
 import java.util.List;
 
+/**
+ * Define las operaciones de acceso a datos para los objetos {@link Empleado}.
+ * Proporciona métodos para insertar, modificar, cambiar la contraseña, eliminar,
+ * obtener por ID, y listar todos los empleados.
+ * <p>
+ * Esta interfaz es un componente clave en el patrón DAO (Data Access Object),
+ * permitiendo la abstracción de las operaciones CRUD (Crear, Leer, Actualizar, y Eliminar)
+ * específicas para la entidad {@code Empleado}.
+ *
+ * @see Empleado
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ */
 public interface EmpleadoDAO {
     int insertar(Empleado empleado);
     int modificar(Empleado empleado);
