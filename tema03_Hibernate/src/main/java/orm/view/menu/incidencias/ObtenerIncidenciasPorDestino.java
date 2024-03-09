8fa7b32e08e8944508216e0eaa3217fb80fee96b
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-09 10:52:19 +0100

    Update ObtenerIncidenciasPorDestino.java

--- tema03_Hibernate/src/main/java/orm/view/menu/incidencias/ObtenerIncidenciasPorDestino.java
+++ tema03_Hibernate/src/main/java/orm/view/menu/incidencias/ObtenerIncidenciasPorDestino.java
@@ -10,12 +10,34 @@ import java.util.List;
 
 import static orm.utilities.Util.*;
 
+/**
+ * Permite la recuperación y visualización de todas las incidencias dirigidas a un empleado específico.
+ * Esta clase facilita la identificación y el seguimiento de incidencias que han sido reportadas con
+ * un empleado como destino, proporcionando una herramienta útil para la revisión de las acciones
+ * o medidas necesarias en respuesta a dichas incidencias.
+ * <p>
+ * La funcionalidad es esencial para mantener una comunicación efectiva y la gestión de incidencias
+ * dentro de una organización, asegurando que los empleados y los gerentes estén informados sobre las
+ * incidencias relevantes.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @see EmpleadosDto
+ * @see IncidenciasDto
+ * @see EmpleadoService
+ * @see IncidenciasService
+ * @see Util
+ */
 public class ObtenerIncidenciasPorDestino {
 
     private static final Util util = new Util();
     private static final EmpleadoService empleadoService = new EmpleadoService();
     private static final IncidenciasService incidenciasService = new IncidenciasService();
 
+    /**
+     * Ejecuta la búsqueda de incidencias destinadas a un empleado específico y muestra sus detalles.
+     * Solicita al usuario el ID del empleado destino y utiliza {@link IncidenciasService} para buscar
+     * y listar las incidencias. Muestra un resumen de cada incidencia encontrada.
+     */
     public static void run() {
         try {
             printlnGreen("--- OBTENER INCIDENCIAS POR DESTINO ---");
