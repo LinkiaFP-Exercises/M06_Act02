ae4d49556ebb57922c4933caa6c20ad5f236a8d7
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-09 10:53:04 +0100

    Update ObtenerIncidenciasPorOrigen.java

--- tema03_Hibernate/src/main/java/orm/view/menu/incidencias/ObtenerIncidenciasPorOrigen.java
+++ tema03_Hibernate/src/main/java/orm/view/menu/incidencias/ObtenerIncidenciasPorOrigen.java
@@ -10,12 +10,34 @@ import java.util.List;
 
 import static orm.utilities.Util.*;
 
+/**
+ * Permite a los usuarios buscar y visualizar las incidencias reportadas por un empleado específico,
+ * usando su ID como criterio de búsqueda. Este proceso es importante para el seguimiento y análisis
+ * de las incidencias originadas por un mismo empleado, permitiendo identificar patrones o áreas de
+ * mejora dentro de la organización.
+ *
+ * Es una herramienta clave para la gestión de incidencias, promoviendo una mayor transparencia y
+ * responsabilidad entre los empleados respecto a las incidencias reportadas.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @see EmpleadosDto
+ * @see IncidenciasDto
+ * @see EmpleadoService
+ * @see IncidenciasService
+ * @see Util
+ */
 public class ObtenerIncidenciasPorOrigen {
 
     private static final Util util = new Util();
     private static final EmpleadoService empleadoService = new EmpleadoService();
     private static final IncidenciasService incidenciasService = new IncidenciasService();
 
+    /**
+     * Realiza la búsqueda y muestra las incidencias reportadas por un empleado origen.
+     * Interactúa con el usuario para obtener el ID del empleado origen y utiliza {@link IncidenciasService}
+     * para recuperar y mostrar las incidencias asociadas. Proporciona detalles relevantes de cada
+     * incidencia encontrada para facilitar su revisión y gestión.
+     */
     public static void run() {
         try {
             printlnGreen("--- OBTENER INCIDENCIAS POR ORIGEN ---");
