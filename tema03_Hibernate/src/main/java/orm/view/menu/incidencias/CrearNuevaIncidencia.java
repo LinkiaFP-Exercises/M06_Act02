0482e6e2fe1654a7dbf6765df4353d9933e7361a
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-09 10:41:52 +0100

    Update CrearNuevaIncidencia.java

--- tema03_Hibernate/src/main/java/orm/view/menu/incidencias/CrearNuevaIncidencia.java
+++ tema03_Hibernate/src/main/java/orm/view/menu/incidencias/CrearNuevaIncidencia.java
@@ -9,11 +9,33 @@ import orm.utilities.Util;
 import static orm.utilities.Util.printLnRed;
 import static orm.utilities.Util.printlnGreen;
 
+/**
+ * Facilita la creación de una nueva incidencia en el sistema.
+ * Solicita al usuario información clave sobre la incidencia, como los empleados involucrados (origen y destino),
+ * el detalle de la incidencia, y el tipo. Esta información se utiliza para crear un registro de incidencia
+ * que se añade a la base de datos a través del {@link IncidenciasService}.
+ * <p>
+ * Este proceso asegura que se recolecten todos los datos necesarios para una adecuada gestión y seguimiento
+ * de las incidencias dentro de la organización.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @see EmpleadosDto
+ * @see IncidenciasDto
+ * @see EmpleadoService
+ * @see IncidenciasService
+ * @see Util
+ */
 public class CrearNuevaIncidencia {
 
     private static final Util util = new Util();
     private static final EmpleadoService empleadoService = new EmpleadoService();
     private static final IncidenciasService incidenciasService = new IncidenciasService();
+
+    /**
+     * Ejecuta la lógica para recoger datos del usuario y registrar una nueva incidencia en el sistema.
+     * Valida la existencia de los empleados involucrados y recopila la información detallada de la incidencia
+     * antes de proceder con su creación.
+     */
     public static void run() {
         try {
             printlnGreen("--- CREAR NUEVA INCIDENCIA ---");
