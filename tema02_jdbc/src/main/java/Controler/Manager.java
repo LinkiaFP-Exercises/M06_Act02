28e2218b1b7fd70b29b959335d99ac55c500e305
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-09 09:15:18 +0100

    Update Manager.java

--- tema02_jdbc/src/main/java/Controler/Manager.java
+++ tema02_jdbc/src/main/java/Controler/Manager.java
@@ -3,10 +3,19 @@ package Controler;
 import config.DatabaseConnectionTest;
 import view.Menu;
 
+/**
+ * Clase principal que inicia la aplicación de gestión de empleados.
+ * Establece la conexión con la base de datos mediante {@link DatabaseConnectionTest}
+ * y lanza el menú principal de la aplicación a través de {@link Menu}.
+ * <p>
+ * Esta clase es el punto de entrada del programa, encargándose de iniciar los componentes necesarios
+ * para que el usuario interactúe con el sistema de gestión de empleados.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ */
 public class Manager {
 
     public static void main(String[] args) {
-
         DatabaseConnectionTest.testConnection();
         Menu.start();
     }
