d855a72be5223516d7bf3f54c8895d01a72577e0
Fauno Guazina <53791776+FaunoGuazina@users.noreply.github.com>
2024-03-09 10:37:22 +0100

    Update InsertarEmpleado.java

--- tema03_Hibernate/src/main/java/orm/view/menu/empleados/InsertarEmpleado.java
+++ tema03_Hibernate/src/main/java/orm/view/menu/empleados/InsertarEmpleado.java
@@ -7,11 +7,29 @@ import orm.utilities.Util;
 import static orm.utilities.Util.printLnRed;
 import static orm.utilities.Util.printlnGreen;
 
+/**
+ * Permite la inserción de nuevos empleados en el sistema.
+ * Solicita al usuario los datos necesarios para registrar un nuevo empleado, como nombre de usuario,
+ * contraseña, nombre completo y teléfono de contacto, y procede con la inserción a través del {@link EmpleadoService}.
+ * <p>
+ * Esta clase es un ejemplo de cómo se implementan operaciones de creación de registros dentro de la aplicación,
+ * asegurando que la información requerida sea recopilada de manera eficiente y segura antes de realizar la inserción.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @see EmpleadosDto
+ * @see EmpleadoService
+ * @see Util
+ */
 public class InsertarEmpleado {
 
     private static final Util util = new Util();
     private static final EmpleadoService empleadoService = new EmpleadoService();
 
+    /**
+     * Ejecuta el proceso de recopilación de datos del nuevo empleado y su inserción en la base de datos.
+     * Interactúa con el usuario para obtener la información necesaria y utiliza {@link EmpleadoService}
+     * para llevar a cabo la operación de inserción.
+     */
     public static void run() {
         try {
             printlnGreen("--- INSERTAR NUEVO EMPLEADO ---");
